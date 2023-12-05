0f5eefd8b5c74d61d28054387d2e592b0723b8b9
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-12-05 22:47:25 +0100

    Registration Form creation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,10 +10,20 @@ use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail("user@gmail.com");
+        $password = $this->hasher->hashPassword($user, '123456');
+        $user->setPassword($password);
+
+        $manager->persist($user);
 
         $manager->flush();
     }
